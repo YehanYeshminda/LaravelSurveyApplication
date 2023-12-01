7b120c82ce1b9b8a5803a364072841c46c61ba88
Yehan Yeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2023-12-01 05:47:05 +0400

    initial page setups with routing, state and layouts completed

--- vue/src/Router/index.js
+++ vue/src/Router/index.js
@@ -2,17 +2,32 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../Views/Login.vue';
 import Register from '../Views/Register.vue';
 import Dashboard from '../Views/Dashboard.vue';
+import DefaultLayout from '../components/DefaultLayout.vue';
+import Survey from '../Views/Survey.vue';
 
 const routes = [
+    {
+        path: '/',
+        redirect: '/dashboard',
+        name: 'Default',
+        component: DefaultLayout,
+        children: [
+            {
+                path: '/dashboard',
+                name: 'Dashboard',
+                component: Dashboard
+            },
+            {
+                path: '/surveys',
+                name: 'Survey',
+                component: Survey
+            },
+        ]
+    },
     {
         path: '/login',
         name: 'Login',
         component: Login
-    },
-    {
-        path: '/dashboard',
-        name: 'Dashboard',
-        component: Dashboard
     },
     {
         path: '/register',
