7ec616ada8b4bb81b7e53615df286112728eecca
Yehan Yeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2023-12-01 16:19:37 +0400

    completed the changes for the default auth and added changes for the state and the management of routes

--- vue/src/Router/index.js
+++ vue/src/Router/index.js
@@ -3,7 +3,9 @@ import Login from '../Views/Login.vue';
 import Register from '../Views/Register.vue';
 import Dashboard from '../Views/Dashboard.vue';
 import DefaultLayout from '../components/DefaultLayout.vue';
+import AuthLayout from '../components/AuthLayout.vue';
 import Survey from '../Views/Survey.vue';
+import store from '../Store';
 
 const routes = [
     {
@@ -11,6 +13,9 @@ const routes = [
         redirect: '/dashboard',
         name: 'Default',
         component: DefaultLayout,
+        meta: {
+            requiresAuth: true
+        },
         children: [
             {
                 path: '/dashboard',
@@ -25,20 +30,41 @@ const routes = [
         ]
     },
     {
-        path: '/login',
-        name: 'Login',
-        component: Login
+        path: '/auth',
+        name: 'Auth',
+        redirect: '/login',
+        meta: {
+            isGuest: true
+        },
+        component: AuthLayout,
+        children: [
+            {
+                path: '/login',
+                name: 'Login',
+                component: Login
+            },
+            {
+                path: '/register',
+                name: 'Register',
+                component: Register
+            }
+        ]
     },
-    {
-        path: '/register',
-        name: 'Register',
-        component: Register
-    }
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth && !store.state.user.token) {
+        next({ name: 'Login' });
+    } else if (store.state.user.token && to.meta.isGuest) {
+        next({ name: 'Dashboard' });
+    } else {
+        next();
+    }
 });
 
 export default router;
